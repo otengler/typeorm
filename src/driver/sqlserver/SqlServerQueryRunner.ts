0e126617e3ae580f0201072a4f803802d6637df0
otengler <oliver.tengler@drivelock.com>
2019-02-18 13:23:50 +0100

    Log query errors

--- src/driver/sqlserver/SqlServerQueryRunner.ts
+++ src/driver/sqlserver/SqlServerQueryRunner.ts
@@ -216,6 +216,7 @@ export class SqlServerQueryRunner extends BaseQueryRunner implements QueryRunner
                     let promiseIndex = this.queryResponsibilityChain.indexOf(promise);
                     let waitingPromiseIndex = this.queryResponsibilityChain.indexOf(waitingPromise);
                     if (err) {
+                        console.error(`SQL statement '${query}' with parameters ${JSON.stringify(parameters)} failed':`, err);
                         this.driver.connection.logger.logQueryError(err, query, parameters, this);
                         resolveChain();
                         return fail(new QueryFailedError(query, parameters, err));
@@ -234,6 +235,7 @@ export class SqlServerQueryRunner extends BaseQueryRunner implements QueryRunner
                 });
 
             } catch (err) {
+                this.driver.connection.logger.logQueryError(err, query, parameters, this);
                 fail(err);
             }
         });
